5a89c9bfedc048c8a4728a775661a93ecbe363c2
frant-quant <frankbeijing3412@gmail.com>
2026-01-19 22:25:06 +0800

    Deploy website

---
+++ assets/js/c15d9823.3fece70d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrank_quant=self.webpackChunkfrank_quant||[]).push([[8146],{29328:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":17,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
